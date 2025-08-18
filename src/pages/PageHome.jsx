ff0de464873de1da5744d350e7f76f26f6f8c452
Arina <arinashkapa@yandex.ru>
2025-08-18 09:55:44 +0200

    catch error lenght movies

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -7,42 +7,48 @@ import { getMovies } from "../API/moviesAPI";
 import { ToastContainer, toast } from "react-toastify";
 
 const PageHome = () => {
-  const [movies, setMovies] = useState([])
-  const [pageFilms, setPageFilms] = useState(1)
-  const [isMoviesLoading, setIsMoviesLoading] = useState(false)
-  
-  const setPageFilmsHandler = () => {
-    if(isMoviesLoading === true){
-        return
-    }
-    setPageFilms(pageFilms + 1)
-  } 
+  const [movies, setMovies] = useState([]);
+  const [pageFilms, setPageFilms] = useState(1);
+  const [isMoviesLoading, setIsMoviesLoading] = useState(false);
+  const [moviesTotal, setMoviesTotal] = useState(0);
 
-  
+  const setPageFilmsHandler = () => {
+    if (isMoviesLoading === true) {
+      return;
+    }
+
+    // отловили ошибку на переполнение запроса фильмов
+    if (movies.length + 10 > moviesTotal) {
+      return;
+    }
+
+    setPageFilms(pageFilms + 1);
+  };
+
   useEffect(() => {
-    setIsMoviesLoading(true)
-    
+    setIsMoviesLoading(true);
+
     getMovies(10, pageFilms)
-    .then((data) => {
-      setMovies([...movies, ...data.docs])
-    })
-    .catch((error) => toast.error('error in getMovies!' + error.message))
-    .finally(() =>  setIsMoviesLoading(false))
+      .then((data) => {
+        setMovies([...movies, ...data.docs]);
+        setMoviesTotal(data.total);
+      })
+      .catch((error) => toast.error("error in getMovies!" + error.message))
+      .finally(() => setIsMoviesLoading(false));
   }, [pageFilms]);
 
   useEffect(() => {
     if (movies.length === 0) {
-      return
+      return;
     }
 
     toast.success(`loaded ${movies.length} movies`);
-  }, [movies])
-  
+  }, [movies]);
 
   /* 
   
-  1. надо понимать когда идет загрузка
-  2. надо понимать если ли ошибки в момент загрузки
+  +1. надо понимать когда идет загрузка
+  +2. надо понимать если ли ошибки в момент загрузки
   3. надо понимать а можем ли мы еще загужать (30) -> 10 10 10 ???
   
   */
